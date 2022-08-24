869a9bf571fd5f516fcaa8519d62f8885273223f
samiulalimsaad <saadnbiuraj@gmail.com>
2022-08-24 20:00:55 +0600

    `express`

---
+++ index.js
@@ -0,0 +1,42 @@
+import cors from "cors";
+import express from "express";
+import { todos } from "./todos.js";
+
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+//  middleWares
+// enable cors
+app.use(cors());
+app.use(express.json());
+
+const nextId = () =>
+    todos.reduce((max, todo) => Math.max(max, todo.id), -1) + 1;
+
+app.get("/", async (req, res) => {
+    res.json(todos);
+});
+app.post("/", async (req, res) => {
+    todos.push({
+        id: nextId(),
+        text: req.body.text,
+        completed: false,
+        color: "",
+    });
+
+    res.json(todos);
+});
+app.delete("/:id", async (req, res) => {
+    const id = req.params.id;
+    todos.splice(+id - 1, 1);
+    res.json(todos);
+});
+app.patch("/:id", async (req, res) => {
+    const id = req?.params?.id;
+    todos[id - 1] = { ...todos[+id - 1], ...req.body };
+    res.json(todos);
+});
+
+app.listen(PORT, async () => {
+    console.log(`server is running at http://localhost:${PORT}`);
+});
